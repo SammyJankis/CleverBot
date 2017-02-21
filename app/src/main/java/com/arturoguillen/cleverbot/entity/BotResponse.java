3132b3ad8f6ad702d82f0af6c40af3123bd9b1f8
SammyJankis <sammyjankis87@gmail.com>
2017-02-21 23:02:00 +0100

    Added change in gitignore

---
+++ app/src/main/java/com/arturoguillen/cleverbot/entity/BotResponse.java
@@ -0,0 +1,8 @@
+package com.arturoguillen.cleverbot.entity;
+
+/**
+ * Created by PC on 21/02/2017.
+ */
+
+public class BotResponse {
+}
--- app/src/main/java/com/arturoguillen/cleverbot/model/CleverBotModel.java
+++ app/src/main/java/com/arturoguillen/cleverbot/model/CleverBotModel.java
@@ -1,8 +1,13 @@
 package com.arturoguillen.cleverbot.model;
 
+import com.arturoguillen.cleverbot.entity.BotResponse;
+
+import java.util.Observable;
+
 import javax.inject.Inject;
 
 import retrofit2.Retrofit;
+import retrofit2.http.GET;
 
 /**
  * Created by PC on 21/02/2017.
@@ -17,5 +22,13 @@ public class CleverBotModel {
 
     }
 
+    private interface CleverBotApi{
+        @GET("")
+        Observable<BotResponse> getReply();
 
+    }
+
+    public void getResponse(){
+        retrofit.create(CleverBotApi.class).getReply();
+    }
 }
