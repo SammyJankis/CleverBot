91243340a9b358724ca74c475649a06805669dde
Arturo Guillen <arturo.guillen@privalia.com>
2017-02-23 18:15:13 +0100

    Added TODO

--- app/src/test/java/com/arturoguillen/cleverbot/TestApp.java
+++ app/src/test/java/com/arturoguillen/cleverbot/TestApp.java
@@ -2,6 +2,8 @@ package com.arturoguillen.cleverbot;
 
 import com.arturoguillen.cleverbot.di.component.ChatComponent;
 import com.arturoguillen.cleverbot.di.component.TestChatComponent;
+import com.arturoguillen.cleverbot.di.module.DataModule;
+import com.arturoguillen.cleverbot.di.module.TestNetModule;
 
 /**
  * Created by arturo.guillen on 23/02/2017.
@@ -14,11 +16,12 @@ public class TestApp extends App {
     @Override
     public void onCreate() {
         super.onCreate();
-        //TODO: Verify that this work
-        /*component = DaggerTestChatComponent.builder()
+
+        //TODO : https://labs.ribot.co.uk/fast-and-reliable-ui-tests-on-android-17c261b8220c#.ty4tejuw2
+        component = DaggerTestChatComponent.builder()
                 .testNetModule(new TestNetModule())
                 .dataModule(new DataModule(this))
-                .build();*/
+                .build();
     }
 
     @Override
